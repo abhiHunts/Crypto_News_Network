014a1e52e7de82853e23981c05318285a865852d
abhiHunts <abhishek626pawar@gmail.com>
2023-01-14 12:08:54 +0530

    Update settings.py

--- crypto_news/settings.py
+++ crypto_news/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zm$1ckz+6amk(*7)1!*jx87(t18=fc%k^a!58d4z^zpwf45*l@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*","https://web-production-18cf.up.railway.app"]
 
 
 # Application definition
